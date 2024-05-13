d9aeb0227cc4b8773d92c8fd618aae92fc122c88
Ankit Tiwari <ankittiwari28032000@gmail.com>
2024-05-13 16:51:23 +0530

    Firy commit

---
+++ app/src/main/java/com/example/myrecipeapp/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.myrecipeapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val _categoriesState = mutableStateOf(RecipeState())
+    val categoriesState : State<RecipeState> = _categoriesState
+    init {
+        fetchCategories()
+    }
+    private fun fetchCategories(){
+        viewModelScope.launch {
+            try {
+                val response = recipeService.getCategories()
+                _categoriesState.value = _categoriesState.value.copy(
+                    loading = false,
+                    list = response.categories,
+                    error = null
+                )
+            }catch (e : Exception){
+                _categoriesState.value.copy(loading = false,
+                    error = "Error fetching Categories : ${e.message}")
+            }
+        }
+    }
+    data class RecipeState(
+        val loading : Boolean = true,
+        val list : List<Category> = emptyList(),
+        var error : String? = null
+    )
+}
